3b9a57fd790168a38f3af0e554ec191471beadf3
Ritesh Yadav <88195857+Riteshy123@users.noreply.github.com>
2022-10-07 12:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var navMenuAnchorTags=document.querySelectorAll('.nav-menu a');
+
+for(var i=0;i<navMenuAnchorTags.length;i++){
+    navMenuAnchorTags[i].addEventListener('click',function(event){
+        event.preventDefault();
+        event.preve
+
+    });
+}
